bcbce88b568f282a5861a68cdf5110d5d5c59ecd
Priyanshu <mpriyanshu0000@gmail.com>
2022-11-09 01:05:00 +0530

    Third Commit

---
+++ compare.js
@@ -0,0 +1,34 @@
+function getData(evt){
+    evt.preventDefault();
+    console.log("Frontend Connected")
+    const handle = document.getElementById("inputPassword3").value;
+    fetch("https://codeforces.com/api/user.info?handles=" + handle).then((response)=>{return response.json();}).then(data => {
+        console.log("API Linked",data.result[0])
+        document.getElementById('name').innerText = data.result[0].email
+        document.getElementById('city').innerText = data.result[0].city
+        document.getElementById('country').innerText = data.result[0].country
+        document.getElementById('MaxRank').innerText = data.result[0].maxRank
+        document.getElementById('MaxRating').innerText = data.result[0].maxRating
+        document.getElementById('handle').innerText = data.result[0].handle
+        document.getElementById('Avatar').setAttribute('src',data.result[0].avatar);
+        
+
+
+        
+        
+    })
+
+
+}
+
+// function show(data){
+//     let deails = `<p>`
+// }
+
+window.onload = () => {
+    document.getElementById("submit-form").addEventListener("submit", getData);
+}
+
+
+
+  
